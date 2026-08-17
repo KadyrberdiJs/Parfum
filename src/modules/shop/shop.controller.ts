acd498197efafa941dea764b4160cefd34e2977b
KadyrberdiJs <sahedowkadyr59@gmail.com>
2026-08-17 16:16:06 +0500

    controller of shop is done

--- src/modules/shop/shop.controller.ts
+++ src/modules/shop/shop.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { ShopService } from './shop.service';
+import { CreateShopDto } from './dto/create-shop.dto';
+import { UpdateShopDto } from './dto/update-shop.dto';
 
-@Controller('shop')
-export class ShopController {}
+@Controller('shops')
+export class ShopController {
+    constructor(private readonly shopService: ShopService) {};
+
+    @Post()
+    create(@Body() dto: CreateShopDto) {
+        return this.shopService.create(dto);
+    };
+
+    @Get()
+    findAll() {
+        return this.shopService.findAll();
+    };
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.shopService.findOne(id);
+    };
+
+    @Patch(':id')
+    update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() dto: UpdateShopDto) {
+            return this.shopService.update(id, dto);
+    };
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.shopService.remove(id);
+    };
+}
